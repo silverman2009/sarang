9f8c025c5d7a63bbfe96fdf0eae76d6f0d9a7406
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-04 01:08:10 -0700

    fix type get otp

--- src/components/auth/Getotp.tsx
+++ src/components/auth/Getotp.tsx
@@ -3,8 +3,17 @@ import Input from '../common/Input'
 import Phone from "@/assets/images/phone.svg"
 import Image from 'next/image'
 import Logo from "@/assets/images/logo.svg"
+import { useFormik } from 'formik'
 
 const Getotp = () => {
+    const formik = useFormik({
+        initialValues:{
+            phone:""
+        },
+        onSubmit:()=>{
+
+        }
+    })
     return (
 
         <div className='rounded-b-[26px] z-[9999] shadow-md text-white bg_header bg_line h-fit flex flex-col py-10 justify-center items-center'>
@@ -19,7 +28,7 @@ const Getotp = () => {
                     </div>
                     <h5 className='font-artin-black text-3xl -mt-1'>خوش اومدین</h5>
                     <div className='mt-14 w-full'>
-                        <Input classInputTag='placeholder:!text-[#fff]' ltr classInput='border-[0.1rem] border-white bg-white/40 text-white' icon={Phone} placeholder='لطفا شماره تماس خود را وارد کنید' />
+                        <Input formik={formik} name='phone' classInputTag='placeholder:!text-[#fff]' ltr classInput='border-[0.1rem] border-white bg-white/40 text-white' icon={Phone} placeholder='لطفا شماره تماس خود را وارد کنید' />
                     </div>
                 </div>
             </div>
