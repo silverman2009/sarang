b131b621df97c518f6e9b9aaa04996293eef4c0f
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-11 10:15:14 +0330

    fix ar scan 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import Menu from "@/components/common/Menu";
 import ProfileInfo from "@/components/common/ProfileInfo";
 import Pay from "@/components/user/pay";
+import useDriverStore from "@/stores/driver-store";
 
 const Home = () => {
+    const { setCode } = useDriverStore()
+    useEffect(() => {
+        setCode("")
+    }, [])
     return (
         <div className="min-h-screen">
             <ProfileInfo />
