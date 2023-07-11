067626f0e5e60e5dcb91ca61bc49441d61bf06dd
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-11 10:04:10 +0330

    fix ar scan

--- src/pages/qr.tsx
+++ src/pages/qr.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import dynamic from "next/dynamic";
 import React from "react";
+import Lottie from "lottie-react";
+import ScanJson from "@/assets/images/qr-code-scan.json"
 import useGetDriverByCityNumberQuery from "@/hooks/query/drivers/useGetDriverByCityNumberQuery";
+import Button from "@/components/common/Button";
 const QrCode = dynamic(() => import("../components/qr/index"), { ssr: false });
 const Scan_qr = () => {
     const [code, setCode] = useState("");
@@ -11,7 +14,16 @@ const Scan_qr = () => {
 
 
 
-    return <>{isLoading ? <p>در حال گرفتن اطلاعات</p> : <QrCode code={code} setCode={setCode}  />}</>;
+    return <>{!isLoading ? <div className="h-screen flex flex-col justify-between items-center">
+        <div className="mt-14">
+            <Lottie animationData={ScanJson} />
+            <p className="text-center  font-artin-bold text-xl">در حال گرفتن اطلاعات راننده..</p>
+        </div>
+        <div className="layout mb-10">
+            <Button classBtn="bg-orange_light text-white " name="بازگشت" />
+        </div>
+    </div> :
+        <QrCode code={code} setCode={setCode} />}</>;
 };
 
 export default Scan_qr;
