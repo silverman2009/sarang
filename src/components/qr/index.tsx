82e42973f5dcd2ed4b252584e6c4fca6157576ce
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-11 09:34:11 +0330

    fix qr camera

--- src/components/qr/index.tsx
+++ src/components/qr/index.tsx
@@ -1,23 +1,29 @@
 import React, { useEffect } from "react";
-import { Html5Qrcode } from "html5-qrcode";
 import Button from "../common/Button";
-import { useRouter } from "next/router";
+import { Html5Qrcode } from "html5-qrcode";
+import { useRouter } from "next/navigation";
+import { PaymentTypeEnum } from "@/helpers/utils/data";
+import useDriverStore from "@/stores/driver-store";
+
+interface Props {
+    setCode: React.Dispatch<React.SetStateAction<string>>
+}
+
+let html5QrCode: any
 const qrConfig = { fps: 10, qrbox: { width: 300, height: 300 } };
 
-interface Props{
-    html5QrCode:any;
-    handleStop:()=>void;
-    onResult:(decodedText:string)=>void;
-}
-const Qr = ({html5QrCode,handleStop,onResult}:Props) => {
+
+const Qr = ({ setCode }: Props) => {
     const router = useRouter()
+    const { setPaymentTypeEnum } = useDriverStore()
+
     useEffect(() => {
         html5QrCode = new Html5Qrcode("reader");
     }, []);
 
     const handleClickAdvanced = () => {
         const qrCodeSuccessCallback = (decodedText: any) => {
-            onResult(decodedText);
+            onResult()
             handleStop();
         };
         html5QrCode.start({ facingMode: "environment" }, qrConfig, qrCodeSuccessCallback);
@@ -29,11 +35,33 @@ const Qr = ({html5QrCode,handleStop,onResult}:Props) => {
         handleClickAdvanced();
     }, []);
 
+
+
+    const handleStop = () => {
+        try {
+            html5QrCode
+                .stop()
+                .then(() => {
+                    html5QrCode.clear();
+                })
+                .catch((err: any) => {
+                    console.log(err.message);
+                });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+
+    const onResult = () => {
+        setPaymentTypeEnum(PaymentTypeEnum.USER_SCAN_QR_CODE)
+        setCode("12345")
+    }
     return (
         <div className="relative h-screen bg-[#00000043]">
             <div id="reader" className="w-full" />
             <div className="absolute w-[90%] left-1/2 -translate-x-1/2 bottom-10">
-                <Button onClick={()=> router.back()} classBtn="bg-orange_light text-white " name="بازگشت" />
+                <Button onClick={() => router.back()} classBtn="bg-orange_light text-white " name="بازگشت" />
             </div>
         </div>
     );
--- src/pages/qr.tsx
+++ src/pages/qr.tsx
@@ -6,32 +6,14 @@ import useDriverStore from "@/stores/driver-store";
 import { PaymentTypeEnum } from "@/helpers/utils/data";
 const QrCode = dynamic(() => import("../components/qr/index"), { ssr: false });
 const Scan_qr = () => {
-    let html5QrCode:any;
-    const { setPaymentTypeEnum } = useDriverStore()
     const [code, setCode] = useState("");
     const { isLoading } = useGetDriverByCityNumberQuery(code);
-    const onResult = (result: string) => {
-        setPaymentTypeEnum(PaymentTypeEnum.USER_SCAN_QR_CODE)
-        setCode("12345")
-    }
 
 
-    const handleStop = () => {
-        try {
-            html5QrCode
-                .stop()
-                .then((res: any) => {
-                    html5QrCode.clear();
-                })
-                .catch((err: any) => {
-                    console.log(err.message);
-                });
-        } catch (err) {
-            console.log(err);
-        }
-    };
 
-    return <>{isLoading ? <p>در حال گرفتن اطلاعات</p> : <QrCode html5QrCode={html5QrCode} handleStop={handleStop}  onResult={onResult} />}</>;
+
+
+    return <>{isLoading ? <p>در حال گرفتن اطلاعات</p> : <QrCode setCode={setCode}  />}</>;
 };
 
 export default Scan_qr;
