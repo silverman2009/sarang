5657d7f5dd3cf829866bd9e4eacca738bc067000
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-11 09:10:33 +0330

    fix qr camera

--- src/components/qr/index.tsx
+++ src/components/qr/index.tsx
@@ -1,37 +1,29 @@
 import React, { useEffect } from "react";
 import { Html5Qrcode } from "html5-qrcode";
 import Button from "../common/Button";
+import { useRouter } from "next/router";
 const qrConfig = { fps: 10, qrbox: { width: 300, height: 300 } };
-const brConfig = { fps: 10, qrbox: { width: 300, height: 300 } };
-let html5QrCode: any;
 
-const Qr = (props: any) => {
+interface Props{
+    html5QrCode:any;
+    handleStop:()=>void;
+    onResult:(decodedText:string)=>void;
+}
+const Qr = ({html5QrCode,handleStop,onResult}:Props) => {
+    const router = useRouter()
     useEffect(() => {
         html5QrCode = new Html5Qrcode("reader");
     }, []);
 
     const handleClickAdvanced = () => {
         const qrCodeSuccessCallback = (decodedText: any) => {
-            props.onResult(decodedText);
+            onResult(decodedText);
             handleStop();
         };
         html5QrCode.start({ facingMode: "environment" }, qrConfig, qrCodeSuccessCallback);
     };
 
-    const handleStop = () => {
-        try {
-            html5QrCode
-                .stop()
-                .then((res: any) => {
-                    html5QrCode.clear();
-                })
-                .catch((err: any) => {
-                    console.log(err.message);
-                });
-        } catch (err) {
-            console.log(err);
-        }
-    };
+
 
     useEffect(() => {
         handleClickAdvanced();
@@ -41,7 +33,7 @@ const Qr = (props: any) => {
         <div className="relative h-screen bg-[#00000043]">
             <div id="reader" className="w-full" />
             <div className="absolute w-[90%] left-1/2 -translate-x-1/2 bottom-10">
-                <Button classBtn="bg-orange_light text-white " name="بازگشت" />
+                <Button onClick={()=> router.back()} classBtn="bg-orange_light text-white " name="بازگشت" />
             </div>
         </div>
     );
--- src/pages/qr.tsx
+++ src/pages/qr.tsx
@@ -6,14 +6,32 @@ import useDriverStore from "@/stores/driver-store";
 import { PaymentTypeEnum } from "@/helpers/utils/data";
 const QrCode = dynamic(() => import("../components/qr/index"), { ssr: false });
 const Scan_qr = () => {
-    const {setPaymentTypeEnum} = useDriverStore()
+    let html5QrCode:any;
+    const { setPaymentTypeEnum } = useDriverStore()
     const [code, setCode] = useState("");
     const { isLoading } = useGetDriverByCityNumberQuery(code);
     const onResult = (result: string) => {
         setPaymentTypeEnum(PaymentTypeEnum.USER_SCAN_QR_CODE)
         setCode("12345")
     }
-    return <>{isLoading ? <p>در حال گرفتن اطلاعات</p> : <QrCode type={"QR"} onResult={onResult} />}</>;
+
+
+    const handleStop = () => {
+        try {
+            html5QrCode
+                .stop()
+                .then((res: any) => {
+                    html5QrCode.clear();
+                })
+                .catch((err: any) => {
+                    console.log(err.message);
+                });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return <>{isLoading ? <p>در حال گرفتن اطلاعات</p> : <QrCode html5QrCode={html5QrCode} handleStop={handleStop}  onResult={onResult} />}</>;
 };
 
 export default Scan_qr;
